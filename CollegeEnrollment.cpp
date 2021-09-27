a24672288af8ebc4379c4ca0dc55120684f93aa9
Vyathan <mattjbrundage@icloud.com>
2021-09-27 19:56:49 -0400

    Complete

--- CollegeEnrollment.cpp
+++ CollegeEnrollment.cpp
@@ -8,3 +8,37 @@
 
 using namespace std;
 
+//Function Prototype
+void displayArray(int enrollments[3][3]);
+
+int main()
+{
+    // Declare Array
+    int enrollments[3][3] = {0};
+
+    // Store Data in Array
+    for (int year = 0; year < 3; year += 1)
+        for (int semester =0; semester < 3; semester += 1)
+        {
+            cout << "Year " << year + 1 << ", Semester " << semester + 1 << " Enrollments: ";
+            cin >> enrollments[year][semester];
+        } // End For
+
+    //Display Array Contents
+    displayArray(enrollments);
+    return 0;
+
+} // End of Main Function
+
+//*****Function Definitions*****
+void displayArray(int enrollments[3][3])
+{
+    cout << endl << "Enrollment by Semester: " << endl;
+    for (int year = 0; year < 3; year += 1)
+    {
+        cout << "Year " << year + 1 << ": " << endl;
+        for (int semester = 0; semester < 3; semester += 1)
+            cout << "  Semester " << semester + 1 << ": " << nums[year][semester] << endl;
+        // End for
+    } // End for
+}  // End of displayArray function
